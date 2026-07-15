ccc5080cdeb75872bbb58d18be4e116b88b1a38f
arpit-mahajan-cs <arpitmahajan54321@gmail.com>
2026-07-15 22:06:42 +0530

    time complerxity exmaple of if and else

---
+++ Time complexity And Space complexity/example 2.py
@@ -0,0 +1,11 @@
+age = 60
+if age >= 80:
+    print("super senior citizen")
+elif age >= 60:
+    print("senior citizen")
+elif age >= 24:
+    print("working")
+elif age >= 16 and age < 24:
+ print("student")
+else:
+ print("child")
